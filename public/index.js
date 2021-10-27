534e6d8e3fae45f0c27a9aefff13a81671c8fd66
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-27 19:17:17 -0400

    finished index.js

--- public/index.js
+++ public/index.js
@@ -78,39 +78,7 @@ myChart = new Chart(ctx, {
 });
 }
 
-function sendTransaction(isAdding) {
-let nameEl = document.querySelector("#t-name");
-let amountEl = document.querySelector("#t-amount");
-let errorEl = document.querySelector(".form .error");
 
-  // validate form
-if (nameEl.value === "" || amountEl.value === "") {
-    errorEl.textContent = "Missing Information";
-    return;
-}
-else {
-    errorEl.textContent = "";
-}
-
-  // create record
-let transaction = {
-    name: nameEl.value,
-    value: amountEl.value,
-    date: new Date().toISOString()
-};
-
-  // if subtracting funds, convert amount to negative number
-if (!isAdding) {
-    transaction.value *= -1;
-}
-
-  // add to beginning of current array of data
-transactions.unshift(transaction);
-
-  // re-run logic to populate ui with new record
-populateChart();
-populateTable();
-populateTotal();
 
   // also send to server
 fetch("/api/transaction", {
