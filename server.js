0196c30277dd7e316c3242640fe91dd2409f815f
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-27 18:42:35 -0400

    worked server.js

--- server.js
+++ server.js
@@ -15,19 +15,3 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-process.env.MONGODB_URI || 'mongodb://localhost/budget',
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-}
-);
-
-// routes
-app.use(require("./routes/api.js"));
-
-app.listen(PORT, () => {
-console.log(`App running on port ${PORT}!`);
-});
