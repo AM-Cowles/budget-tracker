3c375ccb394084c41bccab9c2a9d76b4b354fa5e
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-27 18:58:33 -0400

    worked service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,4 @@
-const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/index.js',
-    "/icons/icon-512x512.png",
-    "/icons/icon-192x192.png",
-    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-    
-];
+
 
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
