39c8c9e5475ce97d99717a2c6877a88ac8aeb3dd
Alyssa Cowles <alyssa.m.cowles@gmail.com>
2021-10-27 18:54:31 -0400

    worked api.js

--- routes/api.js
+++ routes/api.js
@@ -11,15 +11,7 @@ Transaction.create(body)
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
-Transaction.insertMany(body)
-    .then(dbTransaction => {
-    res.json(dbTransaction);
-    })
-    .catch(err => {
-    res.status(404).json(err);
-    });
-});
+
 
 router.get("/api/transaction", (req, res) => {
 Transaction.find({}).sort({date: -1})
